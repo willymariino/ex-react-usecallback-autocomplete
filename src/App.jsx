1de87289a07cdc1271e1f44ed8a49376a424d3e3
willymariino <wg.mariino@gmail.com>
2025-08-14 01:47:28 +0200

    implement search input and API call for product suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
+/*
+
+💡 Premessa: Stai sviluppando un campo di ricerca intelligente simile a quello di Amazon. Quando l'utente digita, una tendina di suggerimenti mostra i prodotti corrispondenti alla ricerca. Per evitare richieste API eccessive, devi ottimizzare la ricerca con il debounce.
+
+📌 Milestone 1: Creare un campo di ricerca e mostrare la lista dei suggerimenti
+Crea un campo di input (<input type="text">) in cui l’utente può digitare.
+
+Effettua una chiamata API a: 
+/products?search=[query]
+
+La query deve essere sostituita con il testo digitato.
+Mostra i risultati API sotto l'input in una tendina di suggerimenti.
+
+Se l'utente cancella il testo, la tendina scompare.
+
+
+Obiettivo: Mostrare suggerimenti dinamici in base alla ricerca dell'utente.
+
+*/
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+async function getProducts(query, setProduct) {
+
+  try {
+    const res = await axios.get(`http://localhost:3333/products?search=${query}`)
+    setProduct(res.data)
+    console.log(res.data)
+
+  }
+
+  catch {
+    console.error("errore nel caricamento dei prodotti")
+  }
+
+  finally {
+    console.log("operazione completata")
+  }
+
+}
+
+
 
 function App() {
 
+  const [query, setQuery] = useState("")
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    getProducts(query, setProduct)
+  }, [query])
 
   return (
     <>
+
+
 
     </>
   )

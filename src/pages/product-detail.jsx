8db15b66c95af67d5d09460774557c2f32fb039a
willymariino <wg.mariino@gmail.com>
2025-08-18 03:00:30 +0200

    Refactor: update ProductDetail component to use async data fetching

--- src/pages/product-detail.jsx
+++ src/pages/product-detail.jsx
@@ -1,8 +1,39 @@
-function Product_detail() {
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+
+async function getProductDetail(id, setProducts) {
+
+    try {
+        const res = await axios.get(`http://localhost:3333/products/${id}`)
+        setProducts(res.data)
+    }
+
+    catch {
+        (error) => console.error("errore nel caricamento dei dettagli del prodotto!! 🙀🙀🙀🙀", error)
+    }
+
+    finally {
+        console.log("operazione terminata")
+    }
+}
+
+function ProductDetail() {
+
+    const [products, setProducts] = useState(null)
+    const { id } = useParams()
+
+
+
+    useEffect(() => {
+        getProductDetail(id, setProducts)
+    }, [id])
 
     return (
+        <>
 
+        </>
     )
 }
 
-export default Product_detail
+export default ProductDetail
